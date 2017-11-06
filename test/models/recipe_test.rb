41a2d823499601e5275a1947c8aaf59896b92494
landroide13 <lazaro02@hotmail.com>
2017-11-07 09:58:07 +1300

    Recipe/Chef vailidates

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new name:"Fried Veggies", description:"Helhtie"
+    @chef = Chef.create chefname:"jean", email:"pt@vn.com"
+    @recipe = @chef.recipes.build name:"Fried Veggies", description:"Helhtie"
+  end
+
+  test "recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "recipe should be valid" do
