191a9fbc3de5cee812cc33cb9a4174d49e90253b
landroide13 <lazaro02@hotmail.com>
2017-11-07 12:04:17 +1300

    index Recipes

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class RecipesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+  def setup
+    @chef = Chef.create! chefname:"tata", email:"tufo@vn.com"
+    @recipe = Recipe.create name:"tiramisu", description:"Mixed creams with chocolate", chef:@chef_id
+    @recipe2 = @chef.recipes.build name:"Musaka", description:"Egg plants with cheese and tomato saurce"
+    @recipe2.save
+  end
+
   test "should get recipes index" do
-    get recipes_url
+    get recipes_path
     assert_response :success
   end
 
+  test "should see recipes listed" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
+  end
 
 end
