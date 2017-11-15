34f404b7872c7d68e157131873c270059539d62f
landroide13 <lazaro02@hotmail.com>
2017-11-15 14:40:52 +1300

    Chef Profile

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -8,6 +8,28 @@ class ChefsController < ApplicationController
     @chefs = Chef.all
   end
 
-  
+  def create
+    @chef = Chef.new get_params
+    if @chef.save
+      flash[:success] = "Chef Successfully created"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    set_params
+  end
+
+  private
+
+  def get_params
+    params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+  end
+
+  def set_params
+    @chef = Chef.find params[:id]
+  end
 
 end
--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -8,10 +8,22 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "reject an invalid signup" do
-
+    get signup_path
+    assert_no_difference "Chef.count" do
+      post chefs_path, params:{chef:{chefname: " ", email: " ", password: "password", password_confirmation: "password"}}
+    end
+    assert_template 'chefs/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
 
-  test "acept valid sign up" do
-    
+  test "accept valid sign up" do
+    get signup_path
+    assert_difference "Chef.count", 1 do
+      post chefs_path, params:{chef:{chefname: "colorado", email: "vergotas@vn.com ", password: "vergotas", password_confirmation: "vergotas"}}
+    end
+    follow_redirect!
+    assert_template 'chefs/show'
+    assert_not flash.empty?
   end
 end
